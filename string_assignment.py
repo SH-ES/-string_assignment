7b9d5d706a9ece2216c57065f814256ed4b66dc9
SH-ES <shahina.hussains@gmail.com>
2025-05-13 13:47:11 +0500

    Add files via upload

---
+++ string_assignment.py
@@ -0,0 +1,49 @@
+# TASK 1
+
+Table1='X' 
+Table2='Y'
+Table3='Z'
+print('Table1, Table2, Table3')
+#Table1 is a variable and its X is its value.
+#Table2 is a variable and its Y is its value.
+#Table3 is a variable and its Z is its value.
+#print (with samll p) is the command to display output on screen under terminal.
+#To print all three values together, they were put together within parentheses surroundedd by quotes.
+#Then it was run.
+
+# TASK 2
+Flower='Jasmine'  #Variable and its value
+sentance='is the national flower of Pakistan.' #Variable and its value
+full_sentence=(Flower + " " + sentance) #To print both variables together, we create a new variable which adds both previous variables. first variable+" "+ second variable. When there is no space between quotations, there is no space between values when printed.
+print(full_sentence) #print the new variable that adds both previous variables
+
+# TASK 3
+first_name='Shahina' #Variable and its value
+last_name='Hussain' #second variable and its value
+full_name=first_name+" "+last_name #To print both variables together, we create a new variable which adds both previous variables. first variable+" "+ second variable. When there is no space between quotations, there is no space between values when printed.
+print(full_name) #print the new variable that adds both previous variables
+
+# TASK 4
+greet="Hi"  #Variable and its value
+print(greet*5) #Variable is multiplied by the number (times) that is required to be repeated.
+# for multiplication * is used.
+
+# TASK 5
+Wow="PythonRocks" #Variable and its value
+print(Wow[0])  #to print the character in the word, print variable mentioning number of character in [].
+# character counting starts with 0. It counts letters, numbers, spaces and symbols.
+# to print last character -1 is used.
+print(Wow[3])
+print(Wow[-1])
+
+
+# TASK 6
+# patriotism="We love Pakistan"
+# print(emotion[3:7])
+# print(emotion[8:])
+
+patriotism="We love Pakistan"
+words=patriotism.split()
+print(words)
+print(words[1])
+print(words[-1])
